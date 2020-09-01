5206b487c8198f148f85cb33c679dfdc3d77d2f3
James Holt <james.holt@hopeforjustice.org>
2020-09-01 17:41:31 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+
+console.log("ready!");
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animation'), // Required
+  path: '/json/writting.json', // Required
+  renderer: 'svg', // Required
+  loop: false, // Optional
+  autoplay: true, // Optional
+});
+
+setTimeout(function() { 
+    $('.save-the-date, .body-text, .date, h2').css('opacity', '1');
+}, 3000);
+
+});
